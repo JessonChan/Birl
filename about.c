c8e341112c8d76ae6ba9f5fdbc9c746d18f1e487
JessonChan <chinachendongpo@gmail.com>
2011-05-05 21:16:03 +0800

    添加窗口图标并修改关于的链接地址
    添加的图标就是当打开窗口后在最上面或最下面的位置看到小图（说不清楚了），链接地址改为了github上的，可能以后用的多少吧。

--- about.c
+++ about.c
@@ -8,6 +8,7 @@ void about()
     GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(ABOUT_ICO, NULL);
 
     GtkWidget *dialog = gtk_about_dialog_new();
+    gtk_window_set_icon_from_file(GTK_WINDOW(dialog),ABOUT_ICO,NULL);
     gtk_about_dialog_set_name(GTK_ABOUT_DIALOG(dialog), "Birl");
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), VERSION);
     gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog),
@@ -15,7 +16,7 @@ void about()
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog),
                                   "Birl is a simple GUI Ruijie client for Ubuntu.\nBirl Is Ruijie for Linux.");
     gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog),
-                                 "http://www.cnblogs.com/JessonChan/");
+                                 "https://github.com/JessonChan/Birl");
     gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), pixbuf);
     g_object_unref(pixbuf), pixbuf = NULL;
     gtk_dialog_run(GTK_DIALOG (dialog));
